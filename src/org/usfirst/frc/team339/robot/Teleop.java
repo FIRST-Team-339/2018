721144fa276a71e21f0281a20e38ca7e309e47bf
CargoXZ <cramosva@yahoo.com>
2018-02-03 16:01:16 -0500

    Changed take picture in teleop

--- src/org/usfirst/frc/team339/robot/Teleop.java
+++ src/org/usfirst/frc/team339/robot/Teleop.java
@@ -133,28 +133,22 @@ public static void periodic ()
         Hardware.cubeManipulator.deployCubeIntake();
 
 
-    // takes a picture with the axis camera when button 7 on the left Operator
-    // is pressed
 
 
 
-    if (Hardware.leftOperator.getRawButton(6)
-            && Hardware.leftOperator.getRawButton(7))
+    // ------------------------------------
+    // takes a picture with the axis camera when buttons 6 + 7 on the left
+    // Operator is pressed
+    // ------------------------------------
+    if (Hardware.leftOperator.getRawButton(6) == true
+            && Hardware.leftOperator.getRawButton(7) == true
 
-
-        // ------------------------------------
-        // takes a picture with the axis camera when buttons 6 + 7 on the left
-        // Operator is pressed
-        // ------------------------------------
-        if (Hardware.leftOperator.getRawButton(6) == true
-                && Hardware.leftOperator.getRawButton(7) == true
-
-                && pictureTakenByButton == false
-                && takePictureByButton == false)
-            {
-            takePictureByButton = true;
-            Hardware.takePictureTimer.start();
-            } // end if
+            && pictureTakenByButton == false
+            && takePictureByButton == false)
+        {
+        takePictureByButton = true;
+        Hardware.takePictureTimer.start();
+        } // end if
 
     if (!(Hardware.leftOperator.getRawButton(6) == true
             && Hardware.leftOperator.getRawButton(7)) == true
